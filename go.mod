363167f8f08981d44f9fd3fd2ff911075ae8d3d7
diego <diegolara93345@gmail.com>
2024-12-28 18:30:38 -0600

    ability to select list

--- go.mod
+++ go.mod
@@ -4,16 +4,14 @@ go 1.23.2
 
 require (
 	github.com/charmbracelet/bubbles v0.20.0
-	github.com/charmbracelet/bubbletea v1.2.4 // indirect
+	github.com/charmbracelet/bubbletea v1.2.4
 )
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/charmbracelet/harmonica v0.2.0 // indirect
-	github.com/charmbracelet/lipgloss v1.0.0 // indirect
+	github.com/charmbracelet/lipgloss v1.0.0
 	github.com/charmbracelet/x/ansi v0.4.5 // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
-	github.com/elastic/go-sysinfo v1.15.0
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -28,17 +26,10 @@ require (
 	golang.org/x/text v0.20.0 // indirect
 )
 
-require (
-	github.com/elastic/go-windows v1.0.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/prometheus/procfs v0.15.1 // indirect
-	github.com/shirou/gopsutil v3.21.11+incompatible
-	howett.net/plist v0.0.0-20181124034731-591f970eefbb // indirect
-)
+require github.com/shirou/gopsutil v3.21.11+incompatible
 
 require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/shirou/gopsutil/v3 v3.24.5
 	github.com/tklauser/go-sysconf v0.3.14 // indirect
 	github.com/tklauser/numcpus v0.8.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
@@ -50,11 +41,10 @@ require (
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/sahilm/fuzzy v0.1.1 // indirect
-	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 )
 
 require (
-	github.com/lrstanley/bubblezone v0.0.0-20240914071701-b48c55a5e78e // indirect
+	github.com/lrstanley/bubblezone v0.0.0-20240914071701-b48c55a5e78e
 	github.com/shirou/gopsutil/v4 v4.24.11
 )
 
--- main.go
+++ main.go
@@ -13,19 +13,21 @@ import (
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	zone "github.com/lrstanley/bubblezone"
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/v4/mem"
 	"github.com/shirou/gopsutil/v4/process"
 )
 
-var docStyle = lipgloss.NewStyle().Margin(0, 0)
+var docStyle = lipgloss.NewStyle().Margin(0, 0).Border(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("#98971a"))
+var docStyle2 = lipgloss.NewStyle().Margin(2, 0, 0, 0).Border(lipgloss.NormalBorder()).Width(20)
 
 type tickMsg time.Time
 
 var list_item_style = lipgloss.NewStyle().
 	Foreground(lipgloss.Color("32")).
 	Height(1).
-	Width(60).Margin(0, 0)
+	Margin(0, 0)
 
 var defaultStyle = lipgloss.NewStyle().
 	BorderStyle(lipgloss.NormalBorder()).
@@ -69,6 +71,7 @@ type model struct {
 	memory         *mem.VirtualMemoryStat
 	utilChart2     streamlinechart.Model
 	list_cpus      list.Model
+	selected_list  int32
 }
 
 func (m model) Init() tea.Cmd {
@@ -78,11 +81,28 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		if msg.String() == "ctrl+c" {
 			return m, tea.Quit
 		}
+
+	case tea.MouseMsg:
+		if msg.Action != tea.MouseActionRelease || msg.Button != tea.MouseButtonLeft {
+			return m, nil
+		}
+		if zone.Get("CPU").InBounds(msg) {
+			m.selected_list = 1
+			docStyle2 = docStyle2.BorderForeground(lipgloss.Color("#98971a"))
+			docStyle = docStyle.BorderForeground(lipgloss.Color("#ffffff"))
+		} else if zone.Get("Other List").InBounds(msg) {
+			m.selected_list = 0
+			docStyle2 = docStyle2.BorderForeground(lipgloss.Color("#ffffff"))
+			docStyle = docStyle.BorderForeground(lipgloss.Color("#98971a"))
+		}
+
+		return m, nil
 
 	case tickMsg:
 		m.utilChart.Push(rand.Float64() * 100.0)
@@ -97,14 +117,18 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		})
 	case tea.WindowSizeMsg:
 		h, v := docStyle.GetFrameSize()
-		m.list.SetSize((msg.Width-h)/2, (msg.Height-v)/3)
+		//_, v2 := list_item_style.GetFrameSize()
+		m.list.SetSize((msg.Width-h)/2, (msg.Height-v)/2)
 		m.utilChart.Resize((msg.Width-h)/2, (msg.Height-v)/3)
-		m.utilChart2.Resize((msg.Width-h)/2, (msg.Height-v)/3)
-		m.list_cpus.SetSize((msg.Width-h)/2, (msg.Height-v)/3)
-		//	h2, v2 := memoryStyle.GetFrameSize()
+		m.utilChart2.Resize((msg.Width-h)/3, (msg.Height-v)/3)
+		m.list_cpus.SetSize((msg.Width-h)/3, (msg.Height-v)/3)
+		h3, _ := list_item_style.GetFrameSize()
+
+		list_item_style = list_item_style.Width((msg.Width - h3) / 2)
+
 		memoryStyle = memoryStyle.
 			Height((msg.Height - v) / 3).
-			Width((msg.Width - h) / 4)
+			Width((msg.Width - h) / 5)
 	}
 
 	m.utilChart, _ = m.utilChart.Update(msg)
@@ -113,23 +137,27 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	m.utilChart2, _ = m.utilChart2.Update(msg)
 	m.utilChart2.DrawAll()
 	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
+	if m.selected_list == 0 {
+		m.list, cmd = m.list.Update(msg)
+	} else if m.selected_list == 1 {
+		m.list_cpus, cmd = m.list_cpus.Update(msg)
+	}
 	return m, cmd
 }
 
 func (m model) View() string {
 	s := docStyle.Render(m.list.View())
 	w := utilHeaderStyle.Render("Utilization")
-	t := docStyle.Render(m.list_cpus.View())
+	t := docStyle2.Render(m.list_cpus.View())
 
 	w += lipgloss.JoinVertical(lipgloss.Bottom, defaultStyle.Render(m.utilChart.View()))
 	total_mem := memoryStyle.Render("Total Memory: " + strconv.Itoa(int(m.memory.Total)/int(math.Pow(1024, 3))) + " GB\n" +
 		"Used Memory:  " + strconv.Itoa(int(m.memory.Available)/int(math.Pow(1024, 3))) + " GB")
 
-	temp_view := lipgloss.JoinHorizontal(lipgloss.Top, total_mem, defaultStyle.Render(m.utilChart2.View()), t)
-	new_view := lipgloss.JoinHorizontal(lipgloss.Top, s, w)
-	new_view += lipgloss.JoinVertical(lipgloss.Center, temp_view)
-	return new_view
+	temp_view := lipgloss.JoinHorizontal(lipgloss.Top, total_mem, defaultStyle.Render(m.utilChart2.View()), zone.Mark("CPU", t))
+	new_view := lipgloss.JoinHorizontal(lipgloss.Top, zone.Mark("Other List", s), w)
+	final_view := lipgloss.JoinVertical(lipgloss.Left, new_view, temp_view)
+	return zone.Scan(final_view)
 }
 
 func retrieveCurrentCPUUtilization(i int32) float64 {
@@ -142,6 +170,8 @@ func retrieveCurrentCPUUtilization(i int32) float64 {
 }
 
 func main() {
+	zone.NewGlobal()
+
 	width := 60
 	height := 26
 	minYValue := 0.0
@@ -193,11 +223,19 @@ func main() {
 	m := model{
 		list: list.New(items, list.DefaultDelegate{Styles: list.DefaultItemStyles{NormalTitle: list_item_style}}, 0, 0), utilChart: slc1,
 		memory: mem, cpuUtilzations: cpuUtilizations, utilChart2: util_chart,
-		list_cpus: list.New(cpu_items, list.DefaultDelegate{Styles: list.DefaultItemStyles{NormalTitle: list_item_style}}, 30, 30),
+		list_cpus:     list.New(cpu_items, list.DefaultDelegate{Styles: list.DefaultItemStyles{NormalTitle: list_item_style}}, 0, 0),
+		selected_list: 0,
 	}
 	m.list.Title = "Active Processes"
 	m.list_cpus.SetShowTitle(false)
-	p := tea.NewProgram(m, tea.WithAltScreen())
+	m.list_cpus.SetShowHelp(false)
+	m.list_cpus.SetShowFilter(false)
+	m.list_cpus.SetShowStatusBar(false)
+	m.list.SetShowHelp(false)
+	m.list.SetShowFilter(false)
+	m.list.SetShowStatusBar(false)
+
+	p := tea.NewProgram(m, tea.WithAltScreen(), tea.WithMouseCellMotion())
 
 	if _, err := p.Run(); err != nil {
 		fmt.Println("Error running program:", err)
